a3da761665aea68cdc12284f9ff721f6d6e9f873
UisPreben <p.paulsen@stud.uis.no>
2019-10-09 14:30:42 +0200

    indent

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
+
 # contains application-wide configuration, and is loaded in __init__.py
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret' # TODO: Use this with wtforms
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'  # TODO: Use this with wtforms
     # ^ https://overiq.com/flask-101/form-handling-in-flask/
     DATABASE = 'database.db'
     UPLOAD_PATH = 'app/static/uploads'
-    ALLOWED_EXTENSIONS = {} # Might use this at some point, probably don't want people to upload any file type
+    ALLOWED_EXTENSIONS = {}  # Might use this at some point, probably don't want people to upload any file type
     # TODO: implement this?
     ## example = https://gist.github.com/dAnjou/2874714
